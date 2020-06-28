e5c0cf3eb8efa543e50c42c6cc8edc56d0c030e3
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2020-06-27 21:28:02 -0700

    modernized

---
+++ src/PCH.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "RE/Skyrim.h"
+#include "REL/Relocation.h"
+#include "SKSE/SKSE.h"
+
+#define DLLEXPORT __declspec(dllexport)
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,41 @@
 ﻿#include "version.h"
 
-#include "SKSE/API.h"
-#include "SKSE/Logger.h"
 
+extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+{
+	SKSE::Logger::OpenRelative(FOLDERID_Documents, L"\\My Games\\Skyrim Special Edition\\SKSE\\MyFirstPlugin.log");
+	SKSE::Logger::SetPrintLevel(SKSE::Logger::Level::kDebugMessage);
+	SKSE::Logger::SetFlushLevel(SKSE::Logger::Level::kDebugMessage);
+	SKSE::Logger::UseLogStamp(true);
 
-extern "C" {
-	bool SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
-	{
-		SKSE::Logger::OpenRelative(FOLDERID_Documents, L"\\My Games\\Skyrim Special Edition\\SKSE\\MyFirstPlugin.log");
-		SKSE::Logger::SetPrintLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::SetFlushLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::UseLogStamp(true);
+	_MESSAGE("MyFirstPlugin v%s", MYFP_VERSION_VERSTRING);
 
-		_MESSAGE("MyFirstPlugin v%s", MYFP_VERSION_VERSTRING);
+	a_info->infoVersion = SKSE::PluginInfo::kVersion;
+	a_info->name = "MyFirstPlugin";
+	a_info->version = MYFP_VERSION_MAJOR;
 
-		a_info->infoVersion = SKSE::PluginInfo::kVersion;
-		a_info->name = "MyFirstPlugin";
-		a_info->version = MYFP_VERSION_MAJOR;
-
-		if (a_skse->IsEditor()) {
-			_FATALERROR("Loaded in editor, marking as incompatible!\n");
-			return false;
-		}
-
-		auto ver = a_skse->RuntimeVersion();
-		if (ver <= SKSE::RUNTIME_1_5_39) {
-			_FATALERROR("Unsupported runtime version %s!\n", ver.GetString().c_str());
-			return false;
-		}
-
-		return true;
+	if (a_skse->IsEditor()) {
+		_FATALERROR("Loaded in editor, marking as incompatible!\n");
+		return false;
 	}
 
-
-	bool SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-	{
-		_MESSAGE("MyFirstPlugin loaded");
-
-		if (!SKSE::Init(a_skse)) {
-			return false;
-		}
-
-		return true;
+	const auto ver = a_skse->RuntimeVersion();
+	if (ver < SKSE::RUNTIME_1_5_39) {
+		_FATALERROR("Unsupported runtime version %s!\n", ver.GetString().c_str());
+		return false;
 	}
-};
+
+	return true;
+}
+
+
+extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+	_MESSAGE("MyFirstPlugin loaded");
+
+	if (!SKSE::Init(a_skse)) {
+		return false;
+	}
+
+	return true;
+}
