3adba0aeee774a5c20211c86e4110da6e5b7efe0
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2020-07-26 12:40:24 -0700

    update

--- src/PCH.h
+++ src/PCH.h
@@ -4,4 +4,14 @@
 #include "REL/Relocation.h"
 #include "SKSE/SKSE.h"
 
+#include <spdlog/sinks/basic_file_sink.h>
+
+#ifndef NDEBUG
+#include <spdlog/sinks/msvc_sink.h>
+#endif
+
+using namespace std::literals;
+
+namespace logger = SKSE::log;
+
 #define DLLEXPORT __declspec(dllexport)
--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,47 @@
 
 extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
-	SKSE::Logger::OpenRelative(FOLDERID_Documents, L"\\My Games\\Skyrim Special Edition\\SKSE\\MyFirstPlugin.log");
-	SKSE::Logger::SetPrintLevel(SKSE::Logger::Level::kDebugMessage);
-	SKSE::Logger::SetFlushLevel(SKSE::Logger::Level::kDebugMessage);
-	SKSE::Logger::UseLogStamp(true);
+	try {
+#ifndef NDEBUG
+		auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+#else
+		auto path = logger::log_directory() / "MyFirstPlugin.log"sv;
+		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path.string(), true);
+#endif
 
-	_MESSAGE("MyFirstPlugin v%s", MYFP_VERSION_VERSTRING);
+		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
 
-	a_info->infoVersion = SKSE::PluginInfo::kVersion;
-	a_info->name = "MyFirstPlugin";
-	a_info->version = MYFP_VERSION_MAJOR;
+#ifndef NDEBUG
+		log->set_level(spdlog::level::trace);
+#else
+		log->set_level(spdlog::level::info);
+		log->flush_on(spdlog::level::warn);
+#endif
 
-	if (a_skse->IsEditor()) {
-		_FATALERROR("Loaded in editor, marking as incompatible!\n");
+		spdlog::set_default_logger(std::move(log));
+		spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+
+		logger::info("MyFirstPlugin v{}"sv, MYFP_VERSION_VERSTRING);
+
+		a_info->infoVersion = SKSE::PluginInfo::kVersion;
+		a_info->name = "MyFirstPlugin";
+		a_info->version = MYFP_VERSION_MAJOR;
+
+		if (a_skse->IsEditor()) {
+			logger::critical("Loaded in editor, marking as incompatible"sv);
+			return false;
+		}
+
+		const auto ver = a_skse->RuntimeVersion();
+		if (ver < SKSE::RUNTIME_1_5_39) {
+			logger::critical("Unsupported runtime version {}", ver.GetString());
+			return false;
+		}
+	} catch (const std::exception& e) {
+		logger::critical(e.what());
 		return false;
-	}
-
-	const auto ver = a_skse->RuntimeVersion();
-	if (ver < SKSE::RUNTIME_1_5_39) {
-		_FATALERROR("Unsupported runtime version %s!\n", ver.GetString().c_str());
+	} catch (...) {
+		logger::critical("caught unknown exception"sv);
 		return false;
 	}
 
@@ -31,9 +53,17 @@ extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Query(const SKSE::QueryInterface* 
 
 extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
-	_MESSAGE("MyFirstPlugin loaded");
+	try {
+		logger::info("MyFirstPlugin loaded");
 
-	if (!SKSE::Init(a_skse)) {
+		if (!SKSE::Init(a_skse)) {
+			return false;
+		}
+	} catch (const std::exception& e) {
+		logger::critical(e.what());
+		return false;
+	} catch (...) {
+		logger::critical("caught unknown exception"sv);
 		return false;
 	}
 
